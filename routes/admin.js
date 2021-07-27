7bb7e59d38e050e19a0f62b6f641ec19c0550159
Bagus Sanjaya Pratama <bagus.bip24@gmail.com>
2021-07-27 12:45:45 +0700

    bank

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const adminController = require('../controllers/adminController');
+const { upload } = require('../middlewares/multer');
 
 router.get('/dashboard', adminController.viewDashboard);
 
@@ -9,8 +10,16 @@ router.post('/category', adminController.addCategory);
 router.put('/category', adminController.editCategory);
 router.delete('/category/:id', adminController.deleteCategory);
 
+// BANK
 router.get('/bank', adminController.viewBank);
+router.post('/bank', upload, adminController.addBank);
+router.put('/bank', upload, adminController.editBank);
+router.delete('/bank/:id', adminController.deleteBank);
+
+// ITEM
 router.get('/item', adminController.viewItem);
+
+// BOOKING
 router.get('/booking', adminController.viewBooking);
 
 module.exports = router;
